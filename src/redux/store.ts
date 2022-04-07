83e44c70700f8df23ac519aafd5784865d8d65b3
Niku Grönberg <gronbergniku@gmail.com>
2022-04-07 13:28:28 +0300

    remove modal redux slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
-import modalReducer from "./modalSlice";
 
 export const store = configureStore({
-  reducer: { app: appReducer, modal: modalReducer },
+  reducer: { app: appReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
